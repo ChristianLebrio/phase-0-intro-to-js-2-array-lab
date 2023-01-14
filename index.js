872b3710f567877321d26dfde48dd3f01a996955
Christian Lebrio <ChristianLebrio@gmail.com>
2023-01-13 19:35:12 -0500

    solved tests

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(input){
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat(input){
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(input){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(input){
+    return cats.shift()
+}
+
+function appendCat(input){
+    const newArray = [...cats, "Broom"]
+    return newArray
+}
+
+function prependCat(input){
+    const newArray = ["Arnold", ...cats]
+    return newArray
+}
+
+function removeLastCat(input){
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(input){
+    return cats.slice(1)
+}
